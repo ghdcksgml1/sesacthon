482a0727c70771e9752b46927d26f8b9ff74ce12
ghdcksgml1 <ghdcksgml2@naver.com>
2023-06-11 01:05:54 +0900

    feat: 테스트를 위해 잠시 권한 풀기

--- src/main/kotlin/com/haechi/sesacthon/config/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/haechi/sesacthon/config/security/SecurityConfiguration.kt
@@ -25,7 +25,7 @@ class SecurityConfiguration(
             .csrf()
             .disable() // csrf 설정을 사용하지 않는다.
             .authorizeHttpRequests() // HttpRequest를 허용하는 Matchers
-            .requestMatchers("/auth/**", "/",
+            .requestMatchers("/auth/**", "/**",
                 "/swagger-ui/**","/swagger-ui.html", "/v3/api-docs/**") // 스웨거 관련 접근 허용
             .permitAll() // 모든 권한 허용
             .anyRequest() // 나머지 요청들은
