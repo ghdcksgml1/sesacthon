04f531e658a9701a1e221c5835a21f3d2132f14e
ghdcksgml1 <ghdcksgml2@naver.com>
2023-06-10 20:54:50 +0900

    feat: S3 Controller, Config, Service

--- src/main/kotlin/com/haechi/sesacthon/config/awsS3/AwsS3Controller.kt
+++ src/main/kotlin/com/haechi/sesacthon/config/awsS3/AwsS3Controller.kt
@@ -1,5 +1,6 @@
 package com.haechi.sesacthon.config.awsS3
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -10,6 +11,7 @@ class AwsS3Controller(
     val awsS3Service: AwsS3Service
 ) {
 
+    @Operation(summary = "이미지 생성기 (무분별한 사용은 하지 말아주세요. 제 돈이 나갑니다.ㅜㅜ")
     @PostMapping("/")
     fun uploadFile(
         @RequestParam("file") file: MultipartFile
